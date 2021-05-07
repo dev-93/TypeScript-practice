9530ba62177cd0b0b795ce55f66b00560d7b633e
Kim Taenam <cjb940103@gmail.com>
2021-05-07 16:11:53 +0900

    기본 타입 정리

---
+++ 1-types/1-1-basic.ts
@@ -0,0 +1,26 @@
+{
+    /* 
+    javascript
+    primitive: number, string, boolean, bigint
+    object: function, array...
+    */
+
+    // number
+    const num:number = 100;
+
+    // string
+    const str:string = 'hello world';
+
+    // boolean
+    const boal:boolean = false;
+
+    // undefined
+    let age: number | undefined;
+    age = undefined;
+    age = 1;
+
+    // null
+    let person: null;
+    let person2: string | null;
+
+}
