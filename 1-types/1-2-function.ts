aa0dcd51ad333ddcb0ac3b7109e6e763b5a93aab
dev-93 <cr000@naver.com>
2021-05-16 20:39:51 +0900

    optional parameter 예제 전

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -28,4 +28,21 @@
             resolve(100);
         });
     }
+
+    //함수 활용할 수 있는 팁
+    // JS => TS
+    // 최신 JS 문법은 JS도 쓸 수 있고, TS에서도 사용할 수 있는 타입이 더해진 함수정의 방법
+
+    // 1. Optional Parameter
+    function printName(firstName:string, lastName:string) {
+        console.log(firstName);
+        console.log(lastName);
+    }
+
+    printName("taenam", "KIM");
+    printName("jihun");
+    // 위에 예제서는 인자 2개를 받아야하기 때문에 에러를 발생한다.
+    // 두번째 인자가 선택사항이 되게 하려면 Optional Parameter를 이용해야한다.
+
+    
 }
