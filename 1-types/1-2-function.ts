915a977acb61fabfef6ef52bc1dc134d0c0a3a26
dev-93 <cr000@naver.com>
2021-06-04 21:23:16 +0900

    함수 타입 이용

--- 1-types/1-2-function.ts
+++ 1-types/1-2-function.ts
@@ -1,33 +1,33 @@
 {
-    // JS
-    function jsAdd(num1, num2) {
-        return num1 + num2;
-    }
+    // // JS
+    // function jsAdd(num1, num2) {
+    //     return num1 + num2;
+    // }
 
-    // TS
-    function add(num1:number, num2:number):number {
-        return num1 + num2;
-    }
+    // // TS
+    // function add(num1:number, num2:number):number {
+    //     return num1 + num2;
+    // }
 
-    // JS
-    function jsFetchNum(id) {
-        // code ...
-        // code ...
-        // code ...
-        return new Promise((resolve, reject) => {
-            resolve(100);
-        });
-    }
+    // // JS
+    // function jsFetchNum(id) {
+    //     // code ...
+    //     // code ...
+    //     // code ...
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
 
-    // TS
-    function tsFetchNum(id:string): Promise<number> {
-        // return 값은 promise이고, (숫자 형태)
-        // code ...
-        // code ...
-        return new Promise((resolve, reject) => {
-            resolve(100);
-        });
-    }
+    // // TS
+    // function tsFetchNum(id:string): Promise<number> {
+    //     // return 값은 promise이고, (숫자 형태)
+    //     // code ...
+    //     // code ...
+    //     return new Promise((resolve, reject) => {
+    //         resolve(100);
+    //     });
+    // }
 
     //함수 활용할 수 있는 팁
     // JS => TS
@@ -43,7 +43,24 @@
     printName("jihun");
     printName("jihun", undefined);
     // 위에 예제서는 인자 2개를 받아야하기 때문에 에러를 발생한다.
-    // 두번째 인자가 선택사항이 되게 하려면 Optional Parameter를 이용해야한다.
-    // Optional Parameter(?)를 이용해서 두번째 인자에 적용하게 되면 이전 오류가 사라진다.
+    // 두번째 인자가 선택사항이 되게 하려면 Optional parameter 이용해야한다.
+    // Optional parameter(?)를 이용해서 두번째 인자에 적용하게 되면 이전 오류가 사라진다.
 
+
+    // Default parameter
+    // 아무것도 전하지 않아도 기본 값으로 설정된다.
+    function printMessage(message: string = "default Message") {
+        console.log(message);
+    };
+
+    printMessage();
+
+    // Rest parameter
+    function addNumbers(...numbers: number[]): number {
+        return numbers.reduce((a,b) => a + b);
+    };
+
+    console.log(addNumbers(1,2));
+    console.log(addNumbers(1,2,3));
+    console.log(addNumbers(1,2,3,4));
 }
