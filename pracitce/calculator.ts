097caf65d00287359e4e648e558249a43642d078
Kim Taenam <cjb940103@gmail.com>
2021-06-09 11:01:42 +0900

    caculator 풀이

--- pracitce/calculator.ts
+++ pracitce/calculator.ts
@@ -7,17 +7,21 @@ console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
  
+type command = 'add' | 'substract' | 'multiply' | 'divide' | 'remainder'
 
-function calculate(name:string, first:number, second:number):any {
-    if (name === "add") {
-        return first + second
-    } else if (name === "substract") {
-        return first - second
-    } else if (name === "multiply") {
-        return first * second
-    } else if (name === "divide") {
-        return first / second
-    } else {
-        return first % second
+function calculate(command:command, first:number, second:number):number {
+    switch (command) {
+        case 'add':
+            return first + second;
+        case 'substract':
+            return first - second
+        case 'multiply':
+            return first * second;
+        case 'divide':
+            return first / second;
+        case 'remainder':
+            return first % second;
+        default:
+            throw Error("unknown Command")
     }
 }
