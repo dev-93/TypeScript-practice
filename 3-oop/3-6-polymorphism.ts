49b1b0ce703e7bf3d63e92695ba1053ecdcf6f91
dev-93 <cr000@naver.com>
2021-06-30 19:25:49 +0900

    다형성 연습 중

---
+++ 3-oop/3-6-polymorphism.ts
@@ -0,0 +1,96 @@
+{
+    type CoffeeCup = {
+        shots:number;
+        hasMilk: boolean;
+    }
+
+    interface CoffeeMaker {
+        makeCoffee(shots:number): CoffeeCup;
+    }
+
+    class CoffeeMachine implements CoffeeMaker {
+        private static BEANS_GRAMM_PER_SHOT:number = 7;
+        private coffeeBeans:number = 0;
+
+        public constructor(coffeeBeans:number) {
+            this.coffeeBeans = coffeeBeans;
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMachine {
+            return new CoffeeMachine(coffeeBeans);
+        }
+
+        fillCoffeeBeans(beans:number) {
+            if(beans < 0) {
+                throw Error("커피콩은 0개 보다 커야되는데~")
+            }
+
+            this.coffeeBeans += beans;
+        }
+
+        clean() {
+            console.log("기계 청소 중...");
+        }
+
+        private grindBeans(shots:number) {
+            console.log(`${shots}만큼 콩을 갈고있어.`);
+            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+                throw new Error("커피콩이 충분하지 않아!");
+            }
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+        }
+
+        private preheat():void {
+            console.log("데우고 있습니다");
+        }
+
+        private extract(shots: number): CoffeeCup {
+            console.log(`${shots}개의 샷을 내리고 있습니다~`);
+            return {
+                shots,
+                hasMilk: false,
+            };
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots);
+        }
+    }
+
+    class CafeLatteMachine extends CoffeeMachine {
+        private steamMilk():void {
+            console.log("우유를 스팀하고 있습니다~~")
+        }
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            this.steamMilk();
+            return {
+                ...coffee,
+                hasMilk:true,
+            }
+        }
+    }
+
+    class SweetCoffeeMaker extends CoffeeMachine {
+        private addSugar(): void {
+            console.log("설탕을 넣고 있어요~~!");
+        }
+
+        makeCoffee(shots: number): CoffeeCup {
+            const coffee = super.makeCoffee(shots);
+            return {
+                ...coffee,
+                hasMilk: false,
+                sugar: true,
+            }
+        }
+    }
+    
+    const machine = new CoffeeMachine(23);
+    const latteMachine = new CafeLatteMachine(23);
+
+    const coffee = latteMachine.makeCoffee(1);
+    console.log(coffee);
+}
