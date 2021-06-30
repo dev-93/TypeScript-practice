90379838286dad8690137bf2a941a335d53646e5
dev-93 <cr000@naver.com>
2021-06-30 19:04:09 +0900

    상속 연습중

--- 3-oop/3-5-inheritance.ts
+++ 3-oop/3-5-inheritance.ts
@@ -8,16 +8,16 @@
         makeCoffee(shots:number): CoffeeCup;
     }
 
-    class CafeLatteMachine {
+    class CoffeeMachine implements CoffeeMaker {
         private static BEANS_GRAMM_PER_SHOT:number = 7;
         private coffeeBeans:number = 0;
 
-        private constructor(coffeeBeans:number) {
+        public constructor(coffeeBeans:number) {
             this.coffeeBeans = coffeeBeans;
         }
 
-        static makeMachine(coffeeBeans: number): coffeeMachine {
-            return new coffeeMachine(coffeeBeans);
+        static makeMachine(coffeeBeans: number): CoffeeMachine {
+            return new CoffeeMachine(coffeeBeans);
         }
 
         fillCoffeeBeans(beans:number) {
@@ -34,62 +34,10 @@
 
         private grindBeans(shots:number) {
             console.log(`${shots}만큼 콩을 갈고있어.`);
-            if(this.coffeeBeans < shots * coffeeMachine.BEANS_GRAMM_PER_SHOT) {
+            if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
                 throw new Error("커피콩이 충분하지 않아!");
             }
-            this.coffeeBeans -= shots * coffeeMachine.BEANS_GRAMM_PER_SHOT;
-        }
-
-        private preheat():void {
-            console.log("데우고 있습니다");
-        }
-
-        private extract(shots: number): CoffeeCup {
-            console.log(`${shots}개의 샷을 내리고 있습니다~`);
-            return {
-                shots,
-                hasMilk: false,
-            };
-        }
-
-        makeCoffee(shots: number): CoffeeCup {
-            this.grindBeans(shots);
-            this.preheat();
-            const coffee = this.extract(shots);
-            return {...coffee, hasMilk: true};
-        }
-    }
-
-    class coffeeMachine implements CoffeeMaker {
-        private static BEANS_GRAMM_PER_SHOT:number = 7;
-        private coffeeBeans:number = 0;
-
-        private constructor(coffeeBeans:number) {
-            this.coffeeBeans = coffeeBeans;
-        }
-
-        static makeMachine(coffeeBeans: number): coffeeMachine {
-            return new coffeeMachine(coffeeBeans);
-        }
-
-        fillCoffeeBeans(beans:number) {
-            if(beans < 0) {
-                throw Error("커피콩은 0개 보다 커야되는데~")
-            }
-
-            this.coffeeBeans += beans;
-        }
-
-        clean() {
-            console.log("기계 청소 중...");
-        }
-
-        private grindBeans(shots:number) {
-            console.log(`${shots}만큼 콩을 갈고있어.`);
-            if(this.coffeeBeans < shots * coffeeMachine.BEANS_GRAMM_PER_SHOT) {
-                throw new Error("커피콩이 충분하지 않아!");
-            }
-            this.coffeeBeans -= shots * coffeeMachine.BEANS_GRAMM_PER_SHOT;
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
         }
 
         private preheat():void {
@@ -110,6 +58,12 @@
             return this.extract(shots);
         }
     }
+
+    class CafeLatteMachine extends CoffeeMachine {}
     
-    const maker: coffeeMachine = coffeeMachine.makeMachine(32);
+    const machine = new CoffeeMachine(23);
+    const latteMachine = new CafeLatteMachine(23);
+
+    const coffee = latteMachine.makeCoffee(1);
+    console.log(coffee);
 }
