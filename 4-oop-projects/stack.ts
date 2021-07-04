84158bad09d1968fae56df672c35d535dcfaa6ca
dev-93 <cr000@naver.com>
2021-07-04 15:03:38 +0900

    stack 구현

---
+++ 4-oop-projects/stack.ts
@@ -0,0 +1,44 @@
+interface Stack {
+    readonly size: number;
+    push(value: string): void;
+    pop(): string;
+}
+
+type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode;
+}
+
+class StackImpl implements Stack {
+    private _size: number = 0;
+    private head?: StackNode;
+
+    get size() {
+        return this._size;
+    }
+
+    push(value: string) {
+        const node: StackNode = {value, next: this.head};
+        this.head = node;
+        this._size++;
+    }
+
+    pop(): string {
+        if (this.head == null) {
+            throw new Error("stack is Empty!");
+        }
+        const node = this.head;
+        this.head = node.next;
+        this._size--;
+        return node.value;
+    }
+}
+
+const stack = new StackImpl();
+stack.push("taenam 1");
+stack.push("jihun 2");
+stack.push("changhan 3");
+
+while (stack.size !== 0) {
+    console.log(stack.pop());
+}
