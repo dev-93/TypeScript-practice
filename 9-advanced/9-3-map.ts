c9b230b32665d4c13ba3088c83a511405f5330ec
dev-93 <cr000@naver.com>
2021-07-10 13:02:25 +0900

    Video type에 description이 추가 된다면, 전부 추가해줘야 한다.

--- 9-advanced/9-3-map.ts
+++ 9-advanced/9-3-map.ts
@@ -2,15 +2,18 @@
     type Video = {
         title: string;
         author: string;
+        description: string;
     }
 
     type VideoOptional = {
         title?: string;
         author?: string;
+        description?: string;
     }
 
     type VideoReadOnly = {
         readonly title: string;
         readonly author: string;
+        readonly description: string;
     }
 }
