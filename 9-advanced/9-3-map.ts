a39d48a485ab423380ffdde59a0efc1ef51045e4
dev-93 <cr000@naver.com>
2021-07-10 13:14:08 +0900

    map type 이용

--- 9-advanced/9-3-map.ts
+++ 9-advanced/9-3-map.ts
@@ -5,15 +5,29 @@
         description: string;
     }
 
-    type VideoOptional = {
-        title?: string;
-        author?: string;
-        description?: string;
+    type Optional<T> = {
+        [P in keyof T]?: T[P] // for..in처럼 key를 도는것과 같다.
+        // P 라는 것은 T 타입의 모든 key들 중에 하나이다.
+        // T object안에 있는 key를 이용해서 value를 정의할 수 있다.
     }
 
-    type VideoReadOnly = {
-        readonly title: string;
-        readonly author: string;
-        readonly description: string;
+    type VideoOptional = Optional<Video>;
+    // Optional에 전달된 Video의 key들을 돈다.
+    const temp:VideoOptional = {
+        title: "today feeling",
+    }; 
+
+    type Animal = {
+        name: string,
+        type: string,
+        age: number,
+    };
+
+    const animal: Optional<Animal> = {
+        name: 'dog',
+        type: 'mammals',
+        age: 26,
     }
+
+    console.log(animal);
 }
