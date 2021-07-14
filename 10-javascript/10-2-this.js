e9dbd6c4cdd9c237e17ce0aeb4a1709bb8abdac3
dev-93 <cr000@naver.com>
2021-07-14 23:13:01 +0900

    js, this

---
+++ 10-javascript/10-2-this.js
@@ -0,0 +1,26 @@
+console.log(this);
+// Window 객체
+
+function test() {
+    console.log(this);
+}
+
+test(); // Window 객체
+console.clear();
+class Counter {
+    count = 0;
+    increase = function () {
+        console.log(this)
+    }
+}
+
+const counter = new Counter();
+counter.increase(); // Counter (class)
+const caller = counter.increase;
+caller(); // undefined
+
+class Bob {};
+
+const bob = new Bob();
+bob.run = counter.increase;
+bob.run(); // Bob
