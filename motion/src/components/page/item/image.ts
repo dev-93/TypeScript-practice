a90668886002ee978249d285be57a9697cf5e7a2
Kim taenam <cjb940103@gmail.com>
2021-07-18 15:48:21 +0900

    image class 수정 및 note todo class 구현

--- motion/src/components/page/item/image.ts
+++ motion/src/components/page/item/image.ts
@@ -2,7 +2,7 @@ import { BaseComponent } from '../../component.js';
 
 export class ImageComponent extends BaseComponent<HTMLElement>{
     constructor(title: string, url: string) {
-        super(`<section class="imageg">
+        super(`<section class="image">
                     <div class="image__holder"><img class="image__thumbnail"></div>
                     <p class="image__title"></p>
                 </section>`);
