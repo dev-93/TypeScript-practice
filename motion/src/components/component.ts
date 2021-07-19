93e5ed7e6b037a90f0439c9241fe3a2b5fdbc583
Kim taenam <cjb940103@gmail.com>
2021-07-20 00:17:43 +0900

    삭제 기능 구현, component에 removeFrom 생성

--- motion/src/components/component.ts
+++ motion/src/components/component.ts
@@ -1,9 +1,11 @@
 export interface Component {
     attachTo(parent: HTMLElement, position?: InsertPosition): void;
+    removeFrom(parent: HTMLElement): void;
 }
 
 export class BaseComponent<T extends HTMLElement> implements Component {
     protected readonly element: T;
+    
     constructor(htmlString:string) {
         const template = document.createElement('template');
         template.innerHTML = htmlString;
@@ -12,5 +14,12 @@ export class BaseComponent<T extends HTMLElement> implements Component {
 
     attachTo(parent: HTMLElement, position: InsertPosition = "afterbegin") {
         parent.insertAdjacentElement(position, this.element);
+    }
+
+    removeFrom(parent: HTMLElement) {
+        if(parent !== this.element.parentElement) {
+            throw new Error("Parent Mismatch!");
+        }
+        parent.removeChild(this.element);
     }
 }
