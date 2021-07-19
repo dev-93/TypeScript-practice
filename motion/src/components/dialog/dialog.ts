d6951648a067458508c3b62f5ab7c6d2db43368a
Kim taenam <cjb940103@gmail.com>
2021-07-20 01:28:24 +0900

    dialog style add

--- motion/src/components/dialog/dialog.ts
+++ motion/src/components/dialog/dialog.ts
@@ -9,11 +9,13 @@ export class InputDialog extends BaseComponent<HTMLElement> implements Composabl
     submitListener?: OnSubmitListener;
 
     constructor() {
-        super(`<section class="dialog">
-                    <button class="close">&times;</button>
-                    <div id="dialog__Body"></div>
-                    <button class="dialog__submit">ADD</button>
-                </section>`);
+        super(`<dialog class="dialog">
+                    <div class="dialog__container">
+                        <button class="close">&times;</button>
+                        <div id="dialog__Body"></div>
+                        <button class="dialog__submit">ADD</button>
+                    </div>
+                </dialog>`);
 
         const closeBtn = this.element.querySelector(".close")! as HTMLButtonElement;
         closeBtn.onclick = () => {
