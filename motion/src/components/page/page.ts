1aac1f4f64387690714a7b9f8c8e0bc6034debe4
Kim taenam <cjb940103@gmail.com>
2021-07-24 01:39:39 +0900

    드래깅, 포지션 바꾸기

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -12,6 +12,7 @@ interface SectionContainer extends Component, Composable {
     setOnCloseListener(listener: OnCloseListener): void;
     setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
     muteChildren(state: "mute" | "unmute"): void;
+    getBoundingRect(): DOMRect;
 }
 
 type SectionContainerConstructor = {
@@ -87,6 +88,10 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Sec
             this.element.classList.remove("mute-children");
         }
     }
+
+    getBoundingRect(): DOMRect {
+        return this.element.getBoundingClientRect();
+    }
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
@@ -117,8 +122,10 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
             return;
         }
         if (this.dragTarget && this.dragTarget !== this.dropTarget) {
+            const dropY = event.clientY;
+            const srcElement = this.dragTarget.getBoundingRect();
             this.dragTarget.removeFrom(this.element);
-            this.dropTarget.attach(this.dragTarget, 'beforebegin');
+            this.dropTarget.attach(this.dragTarget, dropY < srcElement.y ? 'beforebegin' : 'afterend');
         }
     }
 
