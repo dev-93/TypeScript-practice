f65931df7a0056cb5c32a0c92b392ef3aaa3a260
Kim Taenam <cjb940103@gmail.com>
2021-09-03 15:46:27 +0900

    추상화화

--- 3-oop/example.ts
+++ 3-oop/example.ts
@@ -2,9 +2,13 @@
     type CoffeeCup = {
         shots: number,
         hasMilk: boolean
+    };
+
+    interface CoffeeMaker {
+        makeCoffee(shots: number): CoffeeCup;
     }
 
-    class CoffeeMaker {
+    class CoffeeMachine implements CoffeeMaker {
         private static BEANS_GRAMM_PER_SHOT = 7;
         private coffeeBeans = 0;
 
@@ -12,8 +16,8 @@
             this.coffeeBeans = coffeeBeans;
         };
 
-        static makeMachine(coffeeBeans: number):CoffeeMaker {
-            return new CoffeeMaker(coffeeBeans);
+        static makeMachine(coffeeBeans: number):CoffeeMachine {
+            return new CoffeeMachine(coffeeBeans);
         } 
 
         fillCoffeeBeans(beans: number) {
@@ -24,21 +28,44 @@
             this.coffeeBeans += beans;
         }
 
-        makeCoffee(shots:number): CoffeeCup {
-
-            if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT) {
-                throw new Error("Not Enough coffee beans");
+        private grindBeans(shots:number) {
+            console.log(`${shots} 만큼 글라인딩 하고 있습니다`);
+            if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+                throw new Error("Not Enough coffee beans!");
             }
-    
-            this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMM_PER_SHOT;
+
+            this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+        };
+
+        private preheat():void {
+            console.log("데피는 중");
+        };
+
+        private extract(shots:number):CoffeeCup {
+            console.log(`${shots} 만큼 커피 내리는 중`);
 
             return {
                 shots,
                 hasMilk: false,
             }
         }
+
+        makeCoffee(shots:number): CoffeeCup {
+            this.grindBeans(shots);
+            this.preheat();
+            return this.extract(shots);
+        }
     };
 
-    const maker = CoffeeMaker.makeMachine(32);
-    console.log(maker);
+    const maker1:CoffeeMachine = CoffeeMachine.makeMachine(32);
+    maker1.fillCoffeeBeans(32);
+    maker1.makeCoffee(2);
+
+    console.log(maker1)
+    
+    const maker2:CoffeeMaker = CoffeeMachine.makeMachine(32);
+    maker2.fillCoffeeBeans(32);
+    maker2.makeCoffee(2);
+
+    console.log(maker2);
 }
